b77493b1e76e4a8c06cf8c1de09f6fb3cf4c43f0
Alice Park <alicepark0703@gmail.com>
2026-04-18 13:45:53 -0400

    Initial Frontend Commit

--- app/src/components/EligibilityForm.jsx
+++ app/src/components/EligibilityForm.jsx
@@ -1,5 +1,48 @@
 import { useState } from 'react';
 
 export default function EligibilityForm({ onSubmit }) {
+    const [formData, setFormData] = useState({
+        state: "NY",
+        county: "", // or zip code ... ?
+        citizenshipStatus: "US citizen | legal resident | undocumented",
+        householdSize: "", // integer
+        numAdults: "", // integer
+        numChildren: "", // integer
+        pregnancyStatus: "",
+        annualIncome: "", // or monthly income? (only number $)
+        assets: "", 
+        employmentStatus: "full-time | part-time | unemployed | self-employed | retired",
+        hoursPerWeek: "",
+        studentStatus: "Yes | No",
+        studentGroup: "undergraduate | graduate | post-grad | other", //if yes to student status
+        housingStatus: "rent | own | homeless | other",
+        monthlyRent: "", // if rent and in $ (only number)
+        montlyUtilities: "", // if rent and in $ (only number)
+        disabilityStatus: "Yes | No",
+        veteranStatus: "Yes | No",
+        ageOver60: "Yes | No",
+        receivesSSI: "Yes | No",
+    });
+
+    function handleChange(e) { // keeping formDta state in sync as user types and selects things
+        const { name, value } = e.target;
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault(); // prevnt page refresh on submit
+
+        const cleanedData = { // new object with form's values
+            householdSize: parseInt(formData.householdSize),
+            numAdults: parseInt(formData.numAdults),
+            numChildren: parseInt(formData.numChildren),
+            annualIncome: parseFloat(formData.annualIncome),
+
+        }
+    }
+        
     
 }
